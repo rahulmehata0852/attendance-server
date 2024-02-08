686a84704b30b754ccfb8373d6192f470cd027b4
rahul mahato <rahulmahato0852@gmail.com>
2024-02-08 18:56:59 +0530

    nn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express()
 
 
 app.use(cors())
-app.use(express.static(path.join(__dirname, "dist", "index.html")))
+app.use(express.static(path.join(__dirname, "dist")))
 app.use(express.json())
 
 app.use("/api/admin", require("./routes/adminRoute"))
